d6fb806fc538fec940262f76b410738fe3427463
Mikael Lixenstrand <mikael.lixenstrand@erlang-solutions.com>
2015-06-08 15:54:47 +0100

    Initial

---
+++ boshbootstrap/bootstrap.sh
@@ -0,0 +1,24 @@
+function install {
+    echo installing $1
+        shift
+            apt-get -y install "$@" >/dev/null 2>&1
+        }
+
+        apt-get -y update >/dev/null 2>&1
+
+        install 'development tools' build-essential
+
+        install Ruby ruby2.0 ruby2.0-dev
+        update-alternatives --set ruby /usr/bin/ruby2.0 >/dev/null 2>&1
+        update-alternatives --set gem /usr/bin/gem2.0 >/dev/null 2>&1
+
+        install bundler bundler
+
+        install Git git
+        install sqldev libsqlite3-dev
+
+        install 'Nokogiri dependencies' libxml2 libxml2-dev libxslt1-dev zlib1g-dev
+        sudo gem install bosh-bootstrap --no-rdoc --no-ri
+        # Needed for docs generation.
+        update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
+        echo 'Done!'
