2a76c7a72a5d7a80a19da3007450b37d91a5c8c0
Mikael Lixenstrand <mikael.lixenstrand@erlang-solutions.com>
2015-06-09 10:31:23 +0100

    Doc gen

--- Vagrantfile
+++ Vagrantfile
@@ -3,9 +3,10 @@
 
 Vagrant.configure('2') do |config|
     config.vm.box      = 'ubuntu/trusty64'
-    config.vm.hostname = 'gitrubyx'
+    config.vm.hostname = 'devubuntu'
     config.vm.network :forwarded_port, guest: 3000, host: 3000
     config.vm.provision :shell, path: 'bootstrap.sh', keep_color: true
+    config.ssh.forward_agent = true
     config.vm.provider "virtualbox" do |v|
         v.memory = 2048
         v.cpus = 4
--- bootstrap.sh
+++ bootstrap.sh
@@ -8,16 +8,14 @@ function install {
 
         install 'development tools' build-essential
 
-        install Ruby ruby2.2 ruby2.2-dev
-        update-alternatives --set ruby /usr/bin/ruby2.2 >/dev/null 2>&1
-        update-alternatives --set gem /usr/bin/gem2.2 >/dev/null 2>&1
+        install Ruby ruby2.0 ruby2.0-dev
+        update-alternatives --set ruby /usr/bin/ruby2.0 >/dev/null 2>&1
+        update-alternatives --set gem /usr/bin/gem2.0 >/dev/null 2>&1
 
 
-        echo installing Bundler
-        install bundler -N >/dev/null 2>&1
-
-        install Git git
-
-        install 'Nokogiri dependencies' libxml2 libxml2-dev libxslt1-dev
-
+        install Stuff git libsqlite3-dev erlang bwm-ng bundler
+        install 'Nokogiri dependencies' libxml2 libxml2-dev libxslt1-dev zlib1g-dev
+        sudo gem install bosh-bootstrap --no-rdoc --no-ri
+        # Needed for docs generation.
+        update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
         echo 'Done!'
